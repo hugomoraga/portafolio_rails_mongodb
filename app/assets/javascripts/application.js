5b2b0c36a91b439ca3e3eb006e52f85efa9ce9ad
Hugo Moraga <hg.moraga@gmail.com>
2020-06-01 10:44:52 -0400

    make it better3

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,6 @@
 // about supported directives.
 //
 //= require rails-ujs
-//= require popper.js/dist/umd/popper
 //= require bootstrap/dist/js/bootstrap
 //= require turbolinks
 //= require_tree .
